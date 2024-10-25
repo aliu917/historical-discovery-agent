f954e236425e9c18cf8a65d03ec53d33b03ad4db
Angela Liu <aliu917@berkeley.edu>
2024-10-24 19:24:00 -0700

    modify ocr to extract from full textbook and fix section levels

--- src/ocr_script.py
+++ src/ocr_script.py
@@ -10,6 +10,7 @@ import os
 
 def pdf_convert(pdf_string):
 
+    print("start")
     pipeline_options = PdfPipelineOptions()
     pipeline_options.do_ocr=True
     pipeline_options.do_table_structure=True
@@ -20,6 +21,7 @@ def pdf_convert(pdf_string):
         }
     )
 
+    print("converting", pdf_string)
     result = converter.convert(pdf_string)
 
     output_dir = Path("../gha_texts/chapters")
@@ -41,10 +43,13 @@ if __name__ == '__main__':
     # pdf_convert("../Angela Suhas Project Proposal.pdf")
     # pdf_convert("../gha_raw_pdf/Africa7.pdf")
 
-    path = "../gha_raw_pdf/chapters"
-    for filename in os.listdir(path):
-        if filename.split('.')[-1] != "pdf":
-            continue
-        file_path = os.path.join(path, filename)
-        print("converting ", str(file_path))
-        pdf_convert(str(file_path))
+    # path = "../gha_raw_pdf/chapters"
+    # for filename in os.listdir(path):
+    #     if filename.split('.')[-1] != "pdf":
+    #         continue
+    #     file_path = os.path.join(path, filename)
+    #     print("converting ", str(file_path))
+    #     pdf_convert(str(file_path))
+
+    # for volume in ["../gha_raw_pdf/africa6_all.pdf", "../gha_raw_pdf/africa7_all.pdf"]:
+    pdf_convert("../gha_raw_pdf/chapters/africa7_10.pdf")
